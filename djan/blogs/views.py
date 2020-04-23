44e4d31e6fe2d37e6417f11bdbd722ade9088cd3
thanakrit <thanakrit.10197@gmail.com>
2020-04-23 16:18:11 +0700

    layout

--- djan/blogs/views.py
+++ djan/blogs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+
+def hello(request):
+    catagory = ['น้ำตก','ภูเขา','ทะเล','ใกล้เมือง']
+    rating = 4
+    return render(request, 'index.html',
+                  {
+                      'name': 'บทความการท่องเที่ยว',
+                      'lastname': 'ธนากฤต สหชาติชัย',
+                      'tage': catagory,
+                      'rating': rating
+                  })
