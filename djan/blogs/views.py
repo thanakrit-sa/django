1b7e8794319e8ab909229d8e77b323b2e396a94f
thanakrit <thanakrit.10197@gmail.com>
2020-04-26 13:27:17 +0700

    now

--- djan/blogs/views.py
+++ djan/blogs/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import article
 # from django.http import HttpResponse
 
 
 def hello(request):
-    return render(request,'index.html')
+    data = article.objects.all()
+    return render(request, 'index.html',{'post':data})
 
 
 def on(request):
     return render(request, 'page1.html')
 
+
 def writetopic(request):
-    return render(request,'writetopic.html')
+    return render(request, 'writetopic.html')
+
 
 def result(request):
     name = request.POST['nametopic']
     description = request.POST['description']
-    return render(request,'result.html',{'name':name,'description':description})
+    return render(request, 'result.html', {'name': name, 'description': description})
+
 
 def register(request):
-    return render(request,'register.html')
+    return render(request, 'register.html')
+
 
 def member(request):
     username = request.POST['username']
@@ -29,12 +36,26 @@ def member(request):
     password = request.POST['password']
     repassword = request.POST['repassword']
 
-    user = User.objects.create_user(
-        username =username,
-        first_name =firstname,
-        last_name =lastname,
-        email =email,
-        password =password
-    )
-    user.save()
+    if password == repassword:
+        if User.objects.filter(username=username).exists():
+            messages.info(request,'Case1')
+            return redirect('/register')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,'Case2')
+            return redirect('/register')
+        else:
+            user = User.objects.create_user(
+                username=username,
+                first_name=firstname,
+                last_name=lastname,
+                email=email,
+                password=password
+            )
+            user.save()
+            return redirect('/')
+    else:
+        messages.info(request,'Case3')
+        return redirect('/register')
+
+def showmember(request):
     return render(request,'member.html')
--- djan/djan/urls.py
+++ djan/djan/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from blogs import views
-from django.conf.urls import url
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
+    path('',views.hello),
     path('on/', views.on),
     path('writetopic/', views.writetopic),
     path('addtopic',views.result),
     path('register/',views.register),
     path('addmember',views.member),
+    path('member',views.showmember)
 ]
