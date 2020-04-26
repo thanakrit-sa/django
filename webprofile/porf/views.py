eb87c6d5517ba224aabada0223e463d821d8d667
thanakrit <thanakrit.10197@gmail.com>
2020-04-26 17:59:15 +0700

    now

--- webprofile/porf/views.py
+++ webprofile/porf/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.db import models 
-import datetime
 
 def home(request):
-    date = models.DateTimeField(auto_now_add=True, blank=True)
-    return render(request,'index.html',{'date':date})
+    return render(request,'index.html')
+ 
